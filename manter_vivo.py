a9ea02a1d442fcd397b96c89fd7b97af575e9007
Eduardo <csaofs1977@gmail.com>
2025-07-20 22:09:42 +0000

    Algumas adições
    Ehhh, O requirements.txt
    Adicionado e manter_vivo.py atualizado

--- manter_vivo.py
+++ manter_vivo.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from threading import Thread
+import requests
+import time
 
 app = Flask('')
 
@@ -7,6 +9,17 @@ app = Flask('')
 def home():
     return "O bot tá vivo, Felipe!"
 
+def ping_self():
+    while True:
+        try:
+            # Substitua pela URL do Render quando o deploy estiver pronto
+            requests.get("https://SEU-APP.onrender.com")
+        except:
+            pass
+        time.sleep(600)  # A cada 10 minutos
+
 def manter_vivo():
-    t = Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080})
-    t.start()
+    t1 = Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': 8080})
+    t2 = Thread(target=ping_self)
+    t1.start()
+    t2.start()
